0081bdaf52a34e1023cf7e68f77b0e90cf7e2927
Nidhi Sansare <nidhisansare.sits.it@gmail.com>
2025-10-22 00:04:19 +0530

    Inserted 3 platform records into MySQL

---
+++ database/adwise360_static_inserts.sql
@@ -0,0 +1,14 @@
+/*
+AdWise360 Project: Marketing Campaign Database Schema
+Author: Nidhi Yugesh Sansare
+Date: 22-10-2025
+*/
+
+USE adwise360;
+INSERT INTO platforms(platform_id, name)
+VALUES
+(1, 'Google Ads'),
+(2, 'Facebook Ads'),
+(3, 'YouTube Ads');
+
+SELECT * FROM platforms;
